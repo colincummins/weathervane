32a03f1e9115e0727821aac83371b0fb7f4c4e6e
Colin Cummins <all.our.august.sundays@gmail.com>
2024-04-30 18:54:28 -0700

    Update FramedText to make it more abstract (now we can use it for the 'About' section, too). It takes text and an optional header and footer and frames them all.

--- framed_text.py
+++ framed_text.py
@@ -2,24 +2,25 @@ import textwrap
 
 
 class FramedText:
-    def __init__(self, quote, author = "", width=70, quote_width = 60):
-        self.quote = quote
-        self.author = author
+    def __init__(self, text, header = "", footer = "", width=70, text_width = 60):
+        self.header = header
+        self.text = text
+        self.footer = footer
         self.width = width
-        self.quote_width = quote_width
+        self.text_width = text_width
 
     def display(self):
         CORNER = "#"
         VERT = "|"
         HORIZ = "="
-        wrapped_text = textwrap.TextWrapper(width=self.quote_width)
-        lines = wrapped_text.wrap(self.quote)
+        wrapped_text = textwrap.TextWrapper(width=self.text_width, replace_whitespace=False)
+        lines = wrapped_text.wrap(self.text)
 
-        padding = (self.width - 2 - self.quote_width) // 2
+        padding = (self.width - 2 - self.text_width) // 2
 
-        print(CORNER + (HORIZ * (self.width - 2) ) + CORNER)
+        print(CORNER + HORIZ * padding + ((self.header + HORIZ * padding).ljust(self.width - 2 - padding, HORIZ)) +  CORNER)
         print(VERT + " " * (self.width - 2) + VERT)
         for line in lines:
             print(VERT + " " * padding + line.ljust(self.width - padding - 2) + VERT)
         print(VERT + " " * (self.width - 2) + VERT)
-        print(CORNER + ((self.author + HORIZ * padding).rjust(self.width - 2 - padding, HORIZ)) + HORIZ * padding +  CORNER)
+        print(CORNER + ((self.footer + HORIZ * padding).rjust(self.width - 2 - padding, HORIZ)) + HORIZ * padding +  CORNER)
