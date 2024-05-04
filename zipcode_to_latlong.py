0ebdaaa616c062fd1ff4370de3594e4088ae3b1e
Colin Cummins <all.our.august.sundays@gmail.com>
2024-05-04 14:27:31 -0700

    Queries were not specific enough and geocoder was interpreting zipcode as a foreign postal code. Updated to specify United States postal codes only.

--- zipcode_to_latlong.py
+++ zipcode_to_latlong.py
@@ -1,8 +1,17 @@
+import wv_config
+from geopy.geocoders import Nominatim
+
+
 def zipcode_to_latlong(zipcode):
-    #Takes a zipcode and returns a latitude and longitude
-    import wv_config
-    
-    from geopy.geocoders import Nominatim
+    # Takes a zipcode and returns a latitude and longitude
+    nominatim_query = {
+        "postalcode": zipcode,
+        "country": "us"
+    }
     geolocator = Nominatim(user_agent=wv_config.user_agent)
-    location = geolocator.geocode(zipcode)
-    return location.latitude,location.longitude
+    location = geolocator.geocode(query=nominatim_query)
+    return location.latitude, location.longitude
+
+
+if __name__ == "__main__":
+    print(zipcode_to_latlong('20001'))
